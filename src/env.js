fe8c25a3822d84727ed55f1b307b868fa5cad222
Sean O'Connor <sean@soconnor.dev>
2024-10-27 11:14:13 -0700

    Remove zod

--- src/env.js
+++ src/env.js
@@ -1,17 +1,10 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
 
 export const env = createEnv({
   /**
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {
-    DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-  },
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
